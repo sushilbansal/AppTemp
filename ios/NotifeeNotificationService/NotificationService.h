fa3e161bf35a3e6c7e4cc29185004cb24d6892da
Sushil Bansal <bansalsushil@gmail.com>
2024-01-28 17:03:23 +0530

    initial commit of full repository

---
+++ ios/NotifeeNotificationService/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  NotifeeNotificationService
+//
+//  Created by sushil bansal on 28/01/2024.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
--- ios/QeeperApp/AppDelegate.mm
+++ ios/QeeperApp/AppDelegate.mm
@@ -1,11 +1,16 @@
 #import "AppDelegate.h"
-
+// @react-native-firebase/app changes Start
+#import <Firebase.h>
+// @react-native-firebase/app changes End
 #import <React/RCTBundleURLProvider.h>
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  // @react-native-firebase/app changes Start
+  [FIRApp configure];
+  // @react-native-firebase/app changes End
   self.moduleName = @"QeeperApp";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
